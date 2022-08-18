adfd1f84acf2ae9852bf20c3ea006fdf96883cf7
xiexiong <alex.xie1@usmarthk.com>
2022-08-18 11:03:23 +0800

    react-app

---
+++ src/pages/intro.js
@@ -0,0 +1,10 @@
+function Intro(props) {
+    return (
+        <div className="intro" >
+            <h1>{props.title}</h1>
+            <p> This is Intro Page </p>
+        </div>
+    )
+}
+
+export default Intro;
